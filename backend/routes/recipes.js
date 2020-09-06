dae7c822d1a39b797711385d11b7771784346ed5
jsng <jun.shuo95@gmail.com>
2020-09-06 01:40:16 +0100

    Added routes and models to backend

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -20,36 +20,36 @@ router.route('/add').post((req, res) => {
     date,
   });
 
-  newExercise.save()
-  .then(() => res.json('Exercise added!'))
+  newRecipe.save()
+  .then(() => res.json('Recipe added!'))
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/:id').get((req, res) => {
-  Exercise.findById(req.params.id)
-    .then(exercise => res.json(exercise))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+// router.route('/:id').get((req, res) => {
+//   Recipe.findById(req.params.id)
+//     .then(recipes => res.json(recipes))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
 
-router.route('/:id').delete((req, res) => {
-  Exercise.findByIdAndDelete(req.params.id)
-    .then(() => res.json('Exercise deleted.'))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+// router.route('/:id').delete((req, res) => {
+//   Recipe.findByIdAndDelete(req.params.id)
+//     .then(() => res.json('Recipe deleted.'))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
 
-router.route('/update/:id').post((req, res) => {
-  Exercise.findById(req.params.id)
-    .then(exercise => {
-      exercise.username = req.body.username;
-      exercise.description = req.body.description;
-      exercise.duration = Number(req.body.duration);
-      exercise.date = Date.parse(req.body.date);
+// router.route('/update/:id').post((req, res) => {
+//   Recipe.findById(req.params.id)
+//     .then(recipes => {
+//       recipes.recipeName = req.body.recipeName;
+//       recipes.description = req.body.description;
+//       recipes.duration = Number(req.body.duration);
+//       recipes.date = Date.parse(req.body.date);
 
-      exercise.save()
-        .then(() => res.json('Exercise updated!'))
-        .catch(err => res.status(400).json('Error: ' + err));
-    })
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+//       recipes.save()
+//         .then(() => res.json('Recipe updated!'))
+//         .catch(err => res.status(400).json('Error: ' + err));
+//     })
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
 
 module.exports = router;
